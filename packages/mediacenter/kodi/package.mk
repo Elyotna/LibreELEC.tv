8f09e4c62ae6352494761806f609971320d4d58f
chewitt <github@chrishewitt.net>
2018-03-03 13:58:45 +0000

    kodi: updates

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="0c5a35b"
+PKG_VERSION="a705857"
 PKG_SHA256=""
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
