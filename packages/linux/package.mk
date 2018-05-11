d57c911c12897b6fc9bc05f9676cddd22ceb65e5
chewitt <chewitt@libreelec.tv>
2018-05-11 17:28:55 +0400

    linux: bump to 4.17.3

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -50,8 +50,8 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   amlogic-mainline)
-    PKG_VERSION="11454943b264b548e714d8edf932ebf306e5f808" # 4.16.1
-    PKG_SHA256="f56bfeeffb56df161abcaac22bedee278831472a5f9d8e877d1dd840d735d080"
+    PKG_VERSION="83e05c58300605f3ab87819c19d1822f54eff81e" # 4.17.3
+    PKG_SHA256="38c00e2b016a54d2bf475f4fa6ca952b9aad9dddf102d2b5cf5d51919743cdb9"
     PKG_URL="https://github.com/torvalds/linux/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_DIR="$PKG_NAME-$PKG_VERSION*"
     PKG_PATCH_DIRS="default"
