610066feab1463a8e3e3ab15f756c20a0092ce74
Christian Hewitt <christianshewitt@gmail.com>
2018-09-26 09:56:39 +0400

    linux: add amlogic-mainline (4.19.0) kernel

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -34,6 +34,14 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
     PKG_BUILD_PERF="no"
     ;;
+  amlogic-mainline)
+    PKG_VERSION="84df9525b0c27f3ebc2ebb1864fa62a97fdedb7d" # 4.19.0
+    PKG_SHA256="791bc6eafb475eac38c3a0a134da3e1436e61285ecda13c09b952bc318a42a17"
+    PKG_URL="https://github.com/torvalds/linux/archive/$PKG_VERSION.tar.gz"
+    PKG_SOURCE_DIR="$PKG_NAME-$PKG_VERSION*"
+    PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET u-boot-tools-aml:host"
+    PKG_PATCH_DIRS="default"
+    ;;
   rockchip-4.4)
     PKG_VERSION="aa8bacf821e5c8ae6dd8cae8d64011c741659945"
     PKG_SHA256="a2760fe89a15aa7be142fd25fb08ebd357c5d855c41f1612cf47c6e89de39bb3"
